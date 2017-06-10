c3acbee57b8a90bedd572cd6821884cb4d2b867a
Adam Dossa <adam.dossa@gmail.com>
2017-06-10 17:40:52 +0100

    Fixes for latest apk

--- public/bot/bot.js
+++ public/bot/bot.js
@@ -2,6 +2,9 @@
 //Initial welcome message
 //Proof links
 //Remove bogus website
+//Make icons nice
+//Unable to query reddit fix
+//Better error messages - request only on incomplete?
 
 var RegistryABI = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"registrars","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_registrarType","type":"uint8"}],"name":"getDetail","outputs":[{"name":"detail","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_registrarType","type":"string"},{"name":"_registrarDetail","type":"string"},{"name":"_registrar","type":"address"}],"name":"createRegistrar","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_registrarType","type":"uint8"}],"name":"getCost","outputs":[{"name":"cost","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"registrarDetails","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_id","type":"bytes32"},{"name":"_addr","type":"address"},{"name":"_result","type":"string"},{"name":"_message","type":"string"}],"name":"error","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_name","type":"string"},{"name":"_registrarType","type":"uint8"}],"name":"lookupName","outputs":[{"name":"addr","type":"address"},{"name":"proof","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"registrarTypes","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_id","type":"bytes32"},{"name":"_name","type":"string"},{"name":"_addr","type":"address"},{"name":"_proof","type":"string"}],"name":"update","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_addr","type":"address"},{"name":"_registrarType","type":"uint8"}],"name":"lookupAddr","outputs":[{"name":"name","type":"string"},{"name":"proof","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_proof","type":"string"},{"name":"_addr","type":"address"},{"name":"_registrarType","type":"uint8"}],"name":"register","outputs":[{"name":"oracleId","type":"bytes32"}],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_addr","type":"address"},{"indexed":false,"name":"_registrarName","type":"string"},{"indexed":false,"name":"_registrar","type":"address"},{"indexed":false,"name":"_registrarType","type":"uint8"}],"name":"RegistrarUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_addr","type":"address"},{"indexed":false,"name":"_proof","type":"string"},{"indexed":false,"name":"_id","type":"bytes32"},{"indexed":false,"name":"_registrarType","type":"uint8"}],"name":"RegistrationSent","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_addr","type":"address"},{"indexed":false,"name":"_name","type":"string"},{"indexed":false,"name":"_proof","type":"string"},{"indexed":false,"name":"_id","type":"bytes32"},{"indexed":false,"name":"_registrarType","type":"uint8"}],"name":"NameAddressProofRegistered","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_addr","type":"address"},{"indexed":false,"name":"_id","type":"bytes32"},{"indexed":false,"name":"_result","type":"string"},{"indexed":false,"name":"_message","type":"string"},{"indexed":false,"name":"_registrarType","type":"uint8"}],"name":"RegistrarError","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_addr","type":"address"},{"indexed":false,"name":"_mismatchedAddr","type":"address"},{"indexed":false,"name":"_registrarType","type":"uint8"}],"name":"AddressMismatch","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_addr","type":"address"},{"indexed":false,"name":"_funds","type":"uint256"},{"indexed":false,"name":"_cost","type":"uint256"},{"indexed":false,"name":"_registrarType","type":"uint8"}],"name":"InsufficientFunds","type":"event"}];
 var RegistryAddress = "0xad9679bcc32891b6f31cb492d6b6e4057433e42f";
@@ -88,6 +91,19 @@ function registrarEnum(registrarName) {
     }
   }
   return -1;
+}
+
+function wrapStatusWithRequest(statusText, registrarType) {
+  return {
+    "text-message": {
+      type: "request",
+      content: {
+          command: "latestStatus",
+          params: {registrar: "Reddit", status: statusText},
+          text: statusText
+      }
+    }
+  };
 }
 
 function validateParams(params, context) {
@@ -183,7 +199,7 @@ var addressToName = {
       type: status.types.TEXT,
       suggestions: registrarSuggestions,
       placeholder: "Registrar"
-    },{
+    }, {
       name: "addr",
       type: status.types.NUMBER,
       placeholder: "Address"
@@ -207,7 +223,19 @@ function register(params) {
   try {
     var gasCost = Registry.getCost.call(registrarEnum(params.registrar));
     var result = Registry.register.sendTransaction(params.proof, web3.eth.accounts[0], registrarEnum(params.registrar), {from: web3.eth.accounts[0], value: gasCost});
-    return {"text-message": "Proof of \"" + params.proof + "\" has been shipped to the " + params.registrar + " Oraclize contract for validation!\n\nPlease be patient, this may take several minutes!\n\nUse */latestStatus* to check for updates."};
+    return wrapStatusWithRequest("Proof of \"" + params.proof + "\" has been shipped to the " + params.registrar + " Oraclize contract for validation!\n\nPlease be patient, this may take several minutes!\n\nClick to see latest status.", params.registrar);
+    //
+    // return {
+    //   "text-message": {
+    //     type: "request",
+    //     content: {
+    //         command: "latestStatus",
+    //         // text: "Proof of \"" + params.proof + "\" has been shipped to the " + params.registrar + " Oraclize contract for validation!\n\nPlease be patient, this may take several minutes!\n\nClick to see latest status."
+    //         text: "something here!"
+    //     }
+    //   }
+    // };
+    // return {"text-message": "Proof of \"" + params.proof + "\" has been shipped to the " + params.registrar + " Oraclize contract for validation!\n\nPlease be patient, this may take several minutes!\n\nClick to see latest status."};
   } catch (err) {
     return {"text-message": "Error: " + err.message};
   }
@@ -275,7 +303,7 @@ var whoAmI = {
       name: "registrar",
       type: status.types.TEXT,
       suggestions: registrarSuggestions,
-      placeholder: "Select Registrar then hit Send"
+      placeholder: "Registrar"
     }
   ],
   preview: function (params) {
@@ -312,7 +340,7 @@ var details = {
       name: "registrar",
       type: status.types.TEXT,
       suggestions: registrarSuggestions,
-      placeholder: "Select Registrar then hit Send"
+      placeholder: "Registrar"
     }
   ],
   preview: function (params) {
@@ -334,16 +362,20 @@ function latestStatus(params) {
     var events = Registry.allEvents({fromBlock: web3.eth.blockNumber - 10000});
     var allEvents = events.get();
     var noEvents = true;
+    status.sendMessage("Where am I?");
     for (var i = 0; i < allEvents.length; i++) {
-      if (allEvents[i].args['_addr'] == web3.eth.accounts[0]) {
+      if ((allEvents[i].args['_addr'] == web3.eth.accounts[0]) && (allEvents[i].args['_registrarType'] == registrarEnum(params.registrar))) {
         var latestEvent = allEvents[i];
         noEvents = false;
       }
     }
     if (noEvents) {
-      return {"text-message": "You don't have any recent updates!"};
+      return wrapStatusWithRequest("You don't have any recent updates!\n\nClick to refresh!", params.registrar)
+      // return {"text-message": "You don't have any recent updates!"};
     }
-    return {"text-message": "Your latest update is " + latestEvent.event + "\n\nDetails are " + JSON.stringify(latestEvent.args, null, '\t')};
+    // return {"text-message": "Foo"};
+
+    return wrapStatusWithRequest("Your latest update is " + latestEvent.event + "\n\nDetails are " + JSON.stringify(latestEvent.args, null, '\t') + "\n\nClick to refresh!", params.registrar);
   } catch (err) {
     return {"text-message": "Error: " + err.message};
   }
@@ -355,15 +387,40 @@ var latestStatus = {
   color: "#5fc48d",
   title: "Registration Status",
   description: "Get Registration Status",
+  params: [
+    {
+      name: "registrar",
+      type: status.types.TEXT,
+      suggestions: registrarSuggestions,
+      placeholder: "Registrar"
+    }
+  ],
   preview: function (params) {
-    return {
-        markup: status.components.text(
-            {},
-            "What's the latest update on my registration?"
-        )
-    };
+      // return {
+      //     markup: status.components.text(
+      //         {},
+      //         JSON.stringify(params)
+      //     )
+      // };
+
+    if (params.hasOwnProperty("status")) {
+      return {
+          markup: status.components.text(
+              {},
+              params.status
+          )
+      };
+    } else {
+      return {
+          markup: status.components.text(
+              {},
+              "What's the latest update on my registration at " + params.registrar + "?"
+          )
+      };
+    }
   },
   handler: latestStatus
 };
 
 status.command(latestStatus);
+status.response(latestStatus);
